50cce4e866768cca5d41a20f09c4215761407a92
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-18 11:54:08 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Stacks and Queues - Exercise/FastFood.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Fast_Food
+{
+    internal class FastFood
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Console.WriteLine(orders.Max());
+            int ordersCount = orders.Count();
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+                if (foodQuantity >= orders.Peek())
+                {
+                    foodQuantity -= orders.Peek();
+                    orders.Dequeue();
+                }
+            }
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {String.Join(" ", orders)}");
+            }
+        }
+    }
+}
