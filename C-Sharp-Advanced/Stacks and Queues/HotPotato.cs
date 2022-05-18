5ecbf6d45fd75e811a6f22636af444e803d87324
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-18 11:37:17 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Stacks and Queues/HotPotato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Hot_Potato
+{
+    internal class HotPotato
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> kids = new Queue<string>(Console.ReadLine().Split());
+            int potatoCounter = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 1; i < potatoCounter; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+
+        }
+    }
+}
