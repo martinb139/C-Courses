a13e379d33cc8a8d746c21784fe13f71b18efed9
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-18 11:38:12 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Stacks and Queues - Exercise/BasicQueueOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Basic_Queue_Operations
+{
+    internal class BasicQueueOperations
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = nums[0];
+            int s = nums[1];
+            int x = nums[2];
+            int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(integers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}
