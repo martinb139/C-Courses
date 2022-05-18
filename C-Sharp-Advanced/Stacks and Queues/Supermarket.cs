b59751a5230ce61ac23fed47fb7974bf1373a0b3
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-18 11:36:58 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Stacks and Queues/Supermarket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Supermarket
+{
+    internal class Supermarket
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> customers = new Queue<string>();
+            List<string> customersPassed = new List<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    if (customers.Count > 0)
+                    {
+                        int count = customers.Count;
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            customersPassed.Add(customers.Dequeue());
+                        }
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int i = 0; i < customersPassed.Count; i++)
+            {
+                Console.WriteLine(customersPassed[i]);
+            }
+
+            Console.WriteLine($"{customers.Count()} people remaining.");
+        }
+    }
+}
