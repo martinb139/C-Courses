545ed1c18209316336004c5325031f4831d2ad58
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-18 11:36:41 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Stacks and Queues/PrintEvenNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Print_Even_Numbers
+{
+    internal class PrintEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            int count = queue.Count;
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                if(queue.Peek() % 2 == 0)
+                {
+                    numbers.Add(queue.Dequeue());
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
