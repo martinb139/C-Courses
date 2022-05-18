e9a54253abdcd75babcd06015580ed985bb14264
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-18 11:36:24 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Stacks and Queues/MatchingBrackets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> bracketIndex = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+
+                if (ch == '(')
+                {
+                    bracketIndex.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = bracketIndex.Pop();
+                    string substring = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}
