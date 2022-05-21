6a50f29110ba53e19eaacce2e3f2009d860be64d
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-21 12:08:52 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Multidimensional Arrays - Lab/JaggedArrayModification.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Jagged_Array_Modification
+{
+    internal class JaggedArrayModification
+    {
+        static void Main(string[] args)
+        {
+            int rowsLength = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[rowsLength][];
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jaggedArray[row] = new int[elements.Length];
+
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    jaggedArray[row][col] = elements[col];
+                }
+            }
+
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0] != "END")
+            {
+                int row = 0;
+                int col = 0;
+                int value = 0;
+
+                row = int.Parse(command[1]);
+                col = int.Parse(command[2]);
+                value = int.Parse(command[3]);
+
+                if (row < 0 || row >= jaggedArray.Length || col < 0 || col >= jaggedArray[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    command = Console.ReadLine().Split();
+                    continue;
+                }
+
+                if (command[0] == "Add")
+                {
+                    jaggedArray[row][col] += value;
+                }
+                else if (command[0] == "Subtract")
+                {
+                    jaggedArray[row][col] -= value;
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                Console.WriteLine(String.Join(" ", jaggedArray[row]));
+            }
+        }
+    }
+}
