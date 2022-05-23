cbe26947f4f352998cb75870db70edff977fab6a
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-05-23 10:40:38 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Multidimensional Arrays-Exercise/SquaresInMatrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Squares_in_Matrix
+{
+    internal class SquaresInMatrix
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split(' ').Select(int.Parse).Where(x => !x.Equals(' ')).ToArray();
+            char[,] matrix = new char[rowsAndCols[0], rowsAndCols[1]];
+            int counter = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                char[] input = Console.ReadLine().Split().Select(char.Parse).Where(x => !x.Equals(' ')).ToArray();
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = input[cols];
+                }
+            }
+
+            for (int rows = 0; rows < matrix.GetLength(0) - 1; rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1) - 1; cols++)
+                {
+                    if (matrix[rows, cols] == matrix[rows, cols + 1]
+                        && matrix[rows, cols] == matrix[rows + 1, cols]
+                        && matrix[rows, cols] == matrix[rows + 1, cols + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
