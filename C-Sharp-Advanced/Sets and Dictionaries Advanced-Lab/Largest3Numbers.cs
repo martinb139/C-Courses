df7329525c1483f4ee1d5c7dca2559b0b36be08b
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-06-01 12:24:40 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Sets and Dictionaries Advanced-Lab/Largest3Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Largest_3_Numbers
+{
+    internal class Largest3Numbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(n => n).ToArray();
+
+            if (numbers.Length >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
