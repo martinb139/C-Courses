588af59c8177fb893c59a30932135ab747934c22
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-06-06 12:43:52 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Sets and Dictionaries Advanced-Lab/ParkingLot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class ParkingLot
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> carNumbers = new HashSet<string>();
+
+        string[] numberAndCommand = Console.ReadLine()
+            .Split(new string[] { ", " }, StringSplitOptions.None).ToArray();
+
+        while (numberAndCommand[0] != "END")
+        {
+            if(numberAndCommand[0] == "IN")
+            {
+                carNumbers.Add(numberAndCommand[1]);
+            }
+            else if(numberAndCommand[0] == "OUT")
+            {
+                carNumbers.Remove(numberAndCommand[1]);
+            }
+
+            numberAndCommand = Console.ReadLine()
+            .Split(new string[] { ", " }, StringSplitOptions.None).ToArray();
+        }
+
+        if(carNumbers.Count > 0)
+        {
+            foreach (var carNumber in carNumbers)
+            {
+                Console.WriteLine(carNumber);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
