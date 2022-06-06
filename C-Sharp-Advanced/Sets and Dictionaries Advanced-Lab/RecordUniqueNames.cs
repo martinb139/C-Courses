0204e2a383ad2814446feae93572e6a0ae6485fc
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-06-06 12:36:41 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Sets and Dictionaries Advanced-Lab/RecordUniqueNames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Record_Unique_Names
+{
+    internal class RecordUniqueNames
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueNames = new HashSet<string>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+
+            foreach (var name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
