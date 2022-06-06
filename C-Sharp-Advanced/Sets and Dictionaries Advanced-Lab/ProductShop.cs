657988ec6fe0abdf984500287b039c9d55bd62b7
Martin Bengyuzov <99760072+martinb139@users.noreply.github.com>
2022-06-06 11:16:02 +0300

    Add files via upload

---
+++ C-Sharp-Advanced/Sets and Dictionaries Advanced-Lab/ProductShop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Product_Shop
+{
+    internal class ProductShop
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> productShop =
+                new Dictionary<string, Dictionary<string, double>>();
+
+            string[] shopInput = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.None).ToArray();
+
+            while (shopInput[0] != "Revision")
+            {
+                double price = double.Parse(shopInput[2]);
+                if (!productShop.ContainsKey(shopInput[0]))
+                {
+                    productShop.Add(shopInput[0], new Dictionary<string, double>());
+                    productShop[shopInput[0]].Add(shopInput[1], price);
+                }
+                else
+                {
+                    productShop[shopInput[0]].Add(shopInput[1], price);
+                }
+
+                shopInput = Console.ReadLine()
+                    .Split(new string[] { ", " }, StringSplitOptions.None).ToArray();
+            }
+
+            foreach (var shop in productShop.Keys.OrderBy(x => x).ToList())
+            {
+                Console.WriteLine(shop + "->");
+                foreach (var product in productShop[shop])
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
